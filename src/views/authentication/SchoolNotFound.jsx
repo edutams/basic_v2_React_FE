0e125ffde03b7701e650324803ac28bc011bf9dc
chukwudi <nwaforcs@edutams.net>
2026-04-09 11:22:09 +0100

    updated tenent auth

--- src/views/authentication/SchoolNotFound.jsx
+++ src/views/authentication/SchoolNotFound.jsx
@@ -1,29 +1,34 @@
-import { Box, Typography, Button } from '@mui/material';
-import { IconSchool } from '@tabler/icons-react';
+import { Box, Container, Typography, Button } from '@mui/material';
+import ErrorImg from 'src/assets/images/backgrounds/errorimg.svg';
+import { Link } from 'react-router';
 
 const SchoolNotFound = () => {
     return (
         <Box
             display="flex"
             flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-            minHeight="100vh"
-            gap={2}
-            px={3}
+            height="100vh"
             textAlign="center"
+            justifyContent="center"
         >
-            <IconSchool size={64} stroke={1.5} color="#aaa" />
-            <Typography variant="h3" fontWeight={700}>
-                School Not Found
-            </Typography>
-            <Typography variant="body1" color="textSecondary" maxWidth={420}>
-                The school domain you are trying to access does not exist or has not been set up yet.
-                Please check the URL or contact your administrator.
-            </Typography>
-            <Button variant="contained" href="/" sx={{ mt: 1 }}>
-                Go to Homepage
-            </Button>
+            <Container maxWidth="md">
+                <img src={ErrorImg} alt="404" />
+                <Typography align="center" variant="h1" mb={4}>
+                    School Not Exist                </Typography>
+                <Typography align="center" variant="h4" mb={4}>
+                    The school domain you are trying to access does not exist or has not been set up yet.
+                    Please check the URL or contact your administrator.
+                </Typography>
+                <Button
+                    color="primary"
+                    variant="contained"
+                    component={Link}
+                    to="/dashboards/modern"
+                    disableElevation
+                >
+                    Go Back to Home
+                </Button>
+            </Container>
         </Box>
     );
 };

9ec5553360bfed2f9943978c47b973b9277eabfb
Chukwudi <chukwudinwafor34@gmail.com>
2026-06-09 12:56:30 +0100

    updated

--- src/api/tenant/bursary/bursarySettingsApi.js
+++ src/api/tenant/bursary/bursarySettingsApi.js
@@ -19,3 +19,18 @@ export const setActiveSessionTerm = async (termId) => {
     const res = await api.post('/bursary/settings/set_bursary_active_session_term', { value: termId });
     return res.data;
 };
+
+export const fetchBursarySessionTerms = async () => {
+    const res = await api.get('/bursary/settings/fetch_bursary_session_terms');
+    return res.data;
+};
+
+export const fetchActiveCategories = async () => {
+    const res = await api.get('/bursary/settings/fetch_active_categories');
+    return res.data;
+};
+
+export const fetchTermsBySessionTerm = async (sessionTermId) => {
+    const res = await api.get(`/bursary/settings/fetch_terms_by_session_term/${sessionTermId}`);
+    return res.data;
+};

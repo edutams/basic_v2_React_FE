c3654b3afd8e237bcb09d65032d1772e2f3349c8
Version Control <oshosa@edutams.net>
2026-06-30 15:44:04 +0100

    fix

--- src/api/tenant/bursary/transactionApi.js
+++ src/api/tenant/bursary/transactionApi.js
@@ -4,3 +4,13 @@ export const fetchTransactionValues = async () => {
     const res = await api.get('/bursary/transactions/get_online_transaction_values');
     return res.data;
 }
+
+export const fetchOnlineTransactions = async (payload) => {
+    const res = await api.post('/bursary/transactions/fetch_online_transactions', payload);
+    return res.data;
+};
+
+export const fetchOnlineTransactionAnalytics = async (payload) => {
+    const res = await api.post('/bursary/transactions/fetch_online_transaction_analytics', payload);
+    return res.data;
+};

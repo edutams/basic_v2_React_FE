a55960f69b469ea7d9bc5ce4349fdaceff0263bc
Version Control <oshosa@edutams.net>
2026-07-02 16:07:57 +0100

    fix

--- src/api/tenant/bursary/transactionApi.js
+++ src/api/tenant/bursary/transactionApi.js
@@ -29,3 +29,17 @@ export const revenueTransactionAmount = async (payload) => {
     const res = await api.post('/bursary/transactions/revenue/fetch_revenue_transaction_amount', payload);
     return res.data;
 };
+
+export const fetchPrintReceipt = async ({ order_id, user_id, session_term_id }) => {
+    const res = await api.get('/bursary/transactions/print_receipt', {
+        params: { order_id, user_id, session_term_id },
+    });
+    return res.data;
+};
+
+export const checkTransactionStatus = async (id) => {
+    const res = await api.get('/bursary/transactions/update_status', {
+        params: { id },
+    });
+    return res.data;
+};

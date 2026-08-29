544b46d2608862230dc8c5ee4a0df07b913cd06d
JesusPride1 <adewunmieo@edutams.net>
2026-08-29 12:24:03 +0100

    update

--- src/layouts/tenant/SchoolLayout.jsx
+++ src/layouts/tenant/SchoolLayout.jsx
@@ -24,7 +24,8 @@ const PageWrapper = styled('div')(() => ({
   flexGrow: 1,
   flexDirection: 'column',
   zIndex: 1,
-  backgroundColor: 'transparent',
+  // backgroundColor: 'transparent',
+  backgroundColor: '#e4e4e4a9',
   overflowX: 'auto',
 }));
 

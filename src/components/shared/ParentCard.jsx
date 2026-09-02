fe01f6e11d3bb2c828f0199b08ca8bf77c83509b
JesusPride1 <adewunmieo@edutams.net>
2026-09-02 08:50:10 +0100

    rework: layout background color for light and dark mode

--- src/components/shared/ParentCard.jsx
+++ src/components/shared/ParentCard.jsx
@@ -23,8 +23,8 @@ const ParentCard = ({ title, children, footer, codeModel, sx }) => {
             ? '0 6px 24px rgba(0, 0, 0, 0.28)'
             : '0 4px 20px rgba(0, 0, 0, 0.07)',
         ...sx,
-        px:"3px",
-        py:"3px",
+        px: "3px",
+        py: "3px",
       }}
     >
       <CardHeader title={title} action={codeModel} />
